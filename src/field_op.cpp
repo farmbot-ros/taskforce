69407031cd2190861596964db10af4206b02e96e
Trim Bresilla <trim.bresilla@gmail.com>
2025-03-23 03:40:09 +0100

    feat: enhance auction system with detailed bid management
    - Introduce `participants` and `auction_id` variables to store auction details.
    - Replace `auction_timer_` with `timer_publisher_`.
    - Add auction initialization variables `timestamp` and `signature`.
    - Modify the `kv` key-value parameter in auctions to use a specific geojson file.
    - Implement a bid countdown with `bid_count` that cancels the timer when bids are completed.
    - Revise `bid_callback` to only process bids matching the current `auction_id` and ensure agents' bids are accepted only once.

--- src/field_op.cpp
+++ src/field_op.cpp
@@ -11,13 +11,15 @@ using namespace std::chrono_literals;
 class Bidder {
   private:
     rclcpp::Node::SharedPtr node;
-    farmbot_interfaces::msg::KeyValue kv;
+    farmbot_interfaces::msg::Agents participants;
+    int32_t bid_count = 30;
+    std::string auction_id = "1234567890";
 
     rclcpp::Publisher<farmbot_interfaces::msg::Auction>::SharedPtr auction_publisher_;
     rclcpp::Subscription<farmbot_interfaces::msg::Bid>::SharedPtr bid_subscriber_;
     rclcpp::Publisher<farmbot_interfaces::msg::Job>::SharedPtr job_publisher_;
     rclcpp::Publisher<std_msgs::msg::Bool>::SharedPtr auction_end_publisher_;
-    rclcpp::TimerBase::SharedPtr auction_timer_;
+    rclcpp::TimerBase::SharedPtr timer_publisher_;
 
   public:
     ~Bidder() {}
@@ -27,33 +29,40 @@ class Bidder {
             "/job/bid", 10, std::bind(&Bidder::bid_callback, this, std::placeholders::_1));
         job_publisher_ = node->create_publisher<farmbot_interfaces::msg::Job>("/job/job", 10);
         auction_end_publisher_ = node->create_publisher<std_msgs::msg::Bool>("/job/auction_end", 10);
-        auction_timer_ = node->create_wall_timer(1s, std::bind(&Bidder::test_auction, this));
-    }
-
-    void bid_callback(const farmbot_interfaces::msg::Bid::SharedPtr msg) {
-        RCLCPP_INFO(node->get_logger(), "Got bid of %ld from %s", msg->bid, msg->agent.name.c_str());
-        return;
+        timer_publisher_ = node->create_wall_timer(1s, std::bind(&Bidder::test_auction, this));
     }
 
     void test_auction() {
         RCLCPP_INFO(node->get_logger(), "Sending test auction");
-
         farmbot_interfaces::msg::Auction auction;
-        auction.auction_id = "test";
-        auction.job_type = "harvest";
-
-        kv.key = "testq";
-        kv.value = "test1";
-        auction.parameters.push_back(kv);
-
-        kv.key = "testq2";
-        kv.value = "test2";
-        auction.parameters.push_back(kv);
-
-        auction.signature = "test";
         auction.timestamp = rclcpp::Time(0);
-
+        auction.signature = "test";
+        auction.auction_id = auction_id;
+        auction.job_type = "harvest";
+        farmbot_interfaces::msg::KeyValue kv;
+        kv.key = "geojson_file";
+        kv.value = "/doc/code/farmbot/src/trailblazer/config/field4.geojson";
+        auction.parameters.push_back(kv);
         auction_publisher_->publish(auction);
+        bid_count--;
+        if (bid_count <= 0) {
+            RCLCPP_INFO(node->get_logger(), "-------- Bidding finished ----------");
+            timer_publisher_->cancel();
+        }
+    }
+
+    void bid_callback(const farmbot_interfaces::msg::Bid::SharedPtr msg) {
+        if (msg->auction_id != auction_id) {
+            return;
+        }
+        for (const auto &agent : participants.beacons) {
+            if (msg->agent.name == agent.name) {
+                return;
+            }
+            RCLCPP_INFO(node->get_logger(), "%s bids %ld", msg->agent.name.c_str(), msg->bid);
+            participants.beacons.push_back(agent);
+        }
+        return;
     }
 
   private:
